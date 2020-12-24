8628f950b213611042c202da58fc7aa9c9974afc
vivek231 <vivekkr.singh90@gmail.com>
2020-12-24 18:44:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,17 +11,17 @@ import numpy as np
 import time
 import torch.optim as optim
 import os
-from model import COVID19Seg
+from model import LungINFseg
 from torch.autograd import Variable
 
 
-classes = 2
-model  = COVID19Seg(classes=2)
-
+classes = 1
+model  = LungINFseg(classes=1)
+model.cuda()
 model.load_state_dict(torch.load('Net.model'))
 
 
-MainPath='/home/vivek/dataset/covid/test/'
+MainPath='/LungINFseg/test/'
 image_pth='a/'
 gt_path='b/'
 
@@ -35,7 +35,7 @@ for file in os.listdir(MainPath+'/'+image_pth):
         AnnList.append(main_part)
 
 
-TestPath='/home/vivek/dataset/covid/test/'
+TestPath='/LungINFseg/test/'
 ImageList=[]
 
 for file in os.listdir(TestPath+'/'+image_pth):
@@ -44,18 +44,20 @@ for file in os.listdir(TestPath+'/'+image_pth):
         main_part=file.split('.png')[0]
         main_part=main_part+'.png'
 
-for a in (ImageList):
 
+for a in (ImageList):
+    model.eval()
     print(a)
     im = cv2.imread(TestPath + '/' + image_pth + '/' + a)
-    im = cv2.resize(im, (256, 256))
-    
+    h,w,c = im.shape
+    # shape = im.shape
+    im = cv2.resize(im, (256,256))   
     im2 = im.copy()
     im3 = im.copy()
     im = im / 255.0
     im = np.expand_dims(im, 0)
     im = np.transpose(im, (0, 3, 1, 2))
-    im = Variable(torch.FloatTensor(im).cuda())
+    im = Variable(torch.FloatTensor(im)).cuda()
 
     im[:, 0, :, :] = im[:, 0, :, :] - 0.485
     im[:, 1, :, :] = im[:, 1, :, :] - 0.456
@@ -64,13 +66,17 @@ for a in (ImageList):
     im[:, 0, :, :] = im[:, 0, :, :] / 0.229
     im[:, 1, :, :] = im[:, 1, :, :] / 0.224
     im[:, 2, :, :] = im[:, 2, :, :] / 0.225
-    out = model(im)
-    _, out = torch.max(out, 1)
+    e1,e2,e3,e4,out = model(im)
+    #_, out = torch.max(out, 1) # in case of cross entropy loss
+    out = out.type(torch.float) # in case of binary cross entropy loss
+    out = torch.sigmoid(out) # in case of binary cross entropy loss
     out = out.cpu().data.numpy()
-   
+    out = out>0.5
     out = np.squeeze(out)
     loc2 = np.where(out == 0)
     out[loc2] = 0
     out=np.array(out,dtype=np.uint8)
-    im2[loc2]=0
-    cv2.imwrite('out/'+a,255*out)
+    #im2[loc2]=0
+    out = cv2.resize(out,(w,h))
+    cv2.imwrite('out/'+a, 255*out)
+
